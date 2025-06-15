44abe403a456a27627a5d8c47a218ace8d1d163c
Kheder Taleb <khdertaleb1@gmail.com>
2025-06-15 18:18:31 +0300

    feat: search filter implementation

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,27 +1,3 @@
-import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Input } from "@/components/ui/input";
-import { Progress } from "@/components/ui/progress";
-import { Textarea } from "@/components/ui/textarea";
-
 export default function Home() {
-  return <div className="p-4">
-    <div className="flex flex-col gap-y-4">
-    <div>
-      <Button>I am a Button!</Button>
-    </div>
-    <div>
-      <Input placeholder="I am an Input" />
-    </div>
-    <div>
-      <Progress value={50} />
-    </div>
-    <div>
-      <Textarea placeholder={"I am a textarea"} />
-    </div>
-    <div>
-      <Checkbox />
-    </div>
-  </div>
-  </div>
+  return <div className="p-4">HOME PAGE</div>
 }
