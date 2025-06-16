7e9597c8a77ca0ba6e0546f4c0de599d5d63dbd3
Kheder Taleb <khdertaleb1@gmail.com>
2025-06-16 23:52:28 +0300

    feat: implement categories finalization

--- src/app/(app)/(home)/search-filters/category-dropdown.tsx
+++ src/app/(app)/(home)/search-filters/category-dropdown.tsx
@@ -2,13 +2,14 @@
 
 import { Button } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
-import { Category } from '@/payload-types'
 import { useRef, useState } from 'react'
 import { useDropdownPosition } from './use-dropdown-position'
 import { SubcategoryMenu } from './subcategory-menu'
+import { CustomCategory } from '../types'
+import Link from 'next/link'
 
 interface Props {
-  category: Category
+  category: CustomCategory
   isActive?: boolean
   isNavigationHovered?: boolean
 }
@@ -39,9 +40,11 @@ export const CategoryDropdown = ({ category, isActive, isNavigationHovered }: Pr
           className={cn(
             'h-11 px-4 bg-transparent border-transparent rounded-full hover:bg-white hover:border-primary text-black',
             isActive && !isNavigationHovered && 'bg-white border-primary',
+            isOpen &&
+              'bg-white border-primary shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] -translate-x-[4px] -translate-y-[4px]',
           )}
         >
-          {category.name}
+          <Link href={`/${category.slug === 'all' ? '' : category.slug}`}>{category.name}</Link>
         </Button>
         {category.subcategories && category.subcategories.length > 0 && (
           <div
